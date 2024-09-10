6c5803b37b5829f6f4b0832bfc074a311139a26f
Abiy Hailu <abiyhailu295@gmail.com>
2024-09-10 09:53:52 +0300

    tailwind 6

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,10 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <h1 className="text-xl text-blue-800 my-4">Hello, Tailwind CSS!</h1>
+      <UserProfile/>
     </div>
   );
 }
