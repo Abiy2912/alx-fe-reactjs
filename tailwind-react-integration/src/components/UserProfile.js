b070c9420be156ee99d4d162200e2eb248190352
Abiy Hailu <abiyhailu295@gmail.com>
2024-09-10 09:47:11 +0300

    tailwind 4

--- tailwind-react-integration/src/components/UserProfile.js
+++ tailwind-react-integration/src/components/UserProfile.js
@@ -7,7 +7,9 @@ function UserProfile() {
         className="rounded-full w-36 h-36 mx-auto"
       />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 text-base">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }
